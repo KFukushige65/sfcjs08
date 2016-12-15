a5070e48cc469fa265ae432f7b261edc816f839c
KFukushige65 <fukushigekeisuke@net77-dhcp137.sfc.keio.ac.jp>
2016-12-15 09:30:48 +0900

    12/1　最後の

--- scripts/main.js
+++ scripts/main.js
@@ -20,9 +20,50 @@ function fastFord(sec){
     return sec;
 }
 
+function updateVolume(){
+    let slider = document.querySelector("#volume");
+    let value = slider.value;
+    let player = document.querySelector("audio");
+    player.volume = value;    
+}
+
+function configureSeekbar(){
+    let seekbar = document.querySelector("#seek-bar");
+    let player = document.querySelector("audio");
+    seekbar.max = player.duration;
+}
+
+function udpateSeekbar(){
+    let seekbar = document.querySelector("#seek-bar");
+    let player = document.querySelector("audio");
+    seekbar.value = player.currentTime;
+}
+
+function updateCurrentTime(){
+    let seekbar = document.querySelector("#seek-bar");
+    let player = document.querySelector("audio");
+    player.currentTime = seekbar.value;
+}
+
 let playerButton = document.querySelector("#play-button");
 playerButton.addEventListener("click", play);
 
 let pauseButton = document.querySelector("#pause-button");
 pauseButton.addEventListener("click", pause);
+
+let volume = document.querySelector("#volume");
+volume.addEventListener("input", updateVolume);
+
+let player = document.querySelector("audio");
+audio.addEventListener("canplay", configureSeekbar);
+audio.addEventListener("timeupdate", updateSeekbar);
+
+let seekbar = document.querySelector("#seek-bar");
+seekbar.addEventListener("input", updateCurrentTime);
+
+
+
+
+
+
 
